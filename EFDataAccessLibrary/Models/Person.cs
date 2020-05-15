66a3d147d3fa95b8d514214b69c08ea28b54c9a5
Tomek Plewniak <tomekplewniak@gmail.com>
2020-05-15 20:44:26 +0200

    Update 'LastName' property.
    Mark property as required and set max length to 50 characters.

--- EFDataAccessLibrary/Models/Person.cs
+++ EFDataAccessLibrary/Models/Person.cs
@@ -11,6 +11,8 @@ namespace EFDataAccessLibrary.Models
         [MaxLength(50)]
         public string FirstName { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
 
         public int Age { get; set; }
