6e91e2218ab835d6b480029ef2c662c6b4dce87c
Tomek Plewniak <tomekplewniak@gmail.com>
2020-05-15 18:49:02 +0200

    Add new 'People' context.

---
+++ EFDataAccessLibrary/DataAccess/PepoleContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class PepoleContext : DbContext
+    {
+        public PepoleContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
