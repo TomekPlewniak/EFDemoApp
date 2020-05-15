9b376aa923c9acfabd9cc590e9fb4686af8c31bf
Tomek Plewniak <tomekplewniak@gmail.com>
2020-05-15 18:36:43 +0200

    Refactor: Remove unused using.

--- EFDataAccessLibrary/Models/Address.cs
+++ EFDataAccessLibrary/Models/Address.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EFDataAccessLibrary.Models
+﻿namespace EFDataAccessLibrary.Models
 {
     public class Address
     {
