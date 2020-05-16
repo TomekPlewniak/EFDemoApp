61f3edac620de4bf7d3858afa43dc2f052c59f99
Tomek Plewniak <tomekplewniak@gmail.com>
2020-05-16 18:02:14 +0200

    Inject PeopleContext.

--- EFDemoWeb/Pages/Index.cshtml.cs
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFDataAccessLibrary.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,13 +12,20 @@ namespace EFDemoWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PepoleContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PepoleContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
+        {
+
+        }
+
+        private void LoadSampleData()
         {
 
         }
